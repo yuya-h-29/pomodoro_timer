a2dec39237a931f31e3bf857be9342d234a803b2
yuya-h-29 <tzou11291996@gmail.com>
2020-05-27 23:46:48 +0900

    stuck with converting 1500 sec to 25 min.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(
@@ -10,32 +13,64 @@ void main() {
             'Pomodoro Timer',
             textAlign: TextAlign.center,
             style: TextStyle(
-              color: Colors.red[200],
+              color: Colors.red[300],
               fontSize: 45,
               fontFamily: 'Source Sans Pro',
             ),
           ),
           backgroundColor: Colors.orange[100],
         ),
-        body: Timer(),
+        body: countDownTimer(),
       ),
     ),
   );
 }
 
-class Timer extends StatefulWidget {
+class countDownTimer extends StatefulWidget {
   @override
-  _TimerState createState() => _TimerState();
+  _countDownTimerState createState() => _countDownTimerState();
 }
 
-class _TimerState extends State<Timer> {
-  int counter = 10;
+class _countDownTimerState extends State<countDownTimer> {
+  int timeLeftInSec = 100;
+  var timeLeft;
+  Timer timer;
+  int min;
 
-  void startTimer() {
-    setState(() {
-      counter--;
+//  bool hasStarted = false;
+
+//  void startOrStop(){
+//    if (!hasStarted) {
+//      startTimer();
+//      hasStarted = true;
+//    }
+//  }
+
+//  var format = new DateFormat("mm:ss");
+
+  void startOrStop() {
+    if (timeLeftInSec == 0){
+      timeLeftInSec = 100;
+    }
+    timer = Timer.periodic(Duration(seconds: 1), (timer) {
+      setState(() {
+        if (timeLeftInSec > 0) {
+          timeLeftInSec--;
+//          timeLeft= Duration(minutes: timeLeftInSec.inMinutes )
+        } else {
+          timer.cancel();
+        }
+//          hasStarted = true;
+//         else {
+//          timer.cancel();
+////          hasStarted = false;
+//          if (timeLeft > 0){
+//            posedTime = timeLeft;
+//            print(posedTime);
+//          }
+//        }
+      });
     });
-
   }
 
   @override
@@ -45,23 +80,42 @@ class _TimerState extends State<Timer> {
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: <Widget>[
           Text(
-            'You can do it!!',
-            style: TextStyle(
-              fontFamily: 'Chelsea',
-              fontSize: 50,
-              color: Colors.pink[800],
-            ),
-          ),
-          Text(
-            '$counter',
+            '$timeLeft',
             style: TextStyle(
               fontWeight: FontWeight.bold,
               fontSize: 60,
+              fontFamily: 'Chelsea',
             ),
           ),
+          (timeLeftInSec == 100)
+              ? Text(
+                  'Tap Pomotaro\nand\nstart timer!!',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontFamily: 'Chelsea',
+                      fontSize: 50,
+                      color: Colors.pink[800]),
+                )
+              : (timeLeftInSec > 0)
+                  ? Text(
+                      'You can do it!!',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontFamily: 'Chelsea',
+                          fontSize: 50,
+                          color: Colors.pink[800]),
+                    )
+                  : Text(
+                      'Otsukaresama!!',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontFamily: 'Chelsea',
+                          fontSize: 50,
+                          color: Colors.pink[800]),
+                    ),
           FlatButton(
             onPressed: () {
-              startTimer();
+              startOrStop();
             },
             child: Image.asset('images/tomato.png'),
           ),
